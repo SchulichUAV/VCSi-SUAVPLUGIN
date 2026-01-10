6621aa0e515f369b6debeb43045101a10b5889b5
Thomas Mattern <thomasmattern1@gmail.com>
2026-01-10 13:13:00 -0700

    Create RTK directory

---
+++ sw/RTK/RtkModule.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QObject>
+
+class RtkReciever;
+class RtkConverter;
+namespace LmCdl
+{
+    class I_QmlApi;
+}
+
+class RtkModule : public QObject
+{
+    Q_OBJECT
+public:
+    explicit RtkModule(
+        LmCdl::I_QmlApi &qmlApi);
+    virtual ~RtkModule();
+
+private:
+    LmCdl::I_QmlApi &qmlApi_;
+
+    std::unique_ptr<RtkReciever> rtkReciever_;
+    std::unique_ptr<RtkConverter> rtkConverter_;
+};
